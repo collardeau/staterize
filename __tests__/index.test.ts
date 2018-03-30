69ab71f44f30e63d4906839e7300177f23a46b54
Thomas Collardeau <thomas@collardeau.com>
2018-03-30 22:37:13 +0200

    first test

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,5 +1,28 @@
-import subject from '../src/index';
+import t from '../src/index';
 
-test('it works', () => {
-  expect(subject()).toBe('hello world');
+test('creates state', () => {
+  const spy = jest.fn();
+  const r = t({
+    withState: [
+      {
+        name: 'count',
+        val: 0
+      },
+      {
+        name: 'loaded',
+        val: false
+      }
+    ],
+    onChange: spy
+  });
+  expect(r.count).toBe(0);
+  expect(r.loaded).toBe(false);
+  r.setCount(1);
+  expect(spy).toHaveBeenCalledWith({
+    count: 1
+  });
+  r.setLoaded(true);
+  expect(spy).toHaveBeenCalledWith({
+    loaded: true
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,39 @@
-function hi() {
-  return 'hello world';
+interface Obj {
+  [name: string]: any;
 }
-export default hi;
+
+interface StateDef {
+  name: string;
+  val: any;
+}
+
+interface Params {
+  withState: StateDef[];
+  onChange: () => Obj;
+}
+
+function reduceState(StateDefs: StateDef[], cb: Function) {
+  return StateDefs.reduce((acc, next) => {
+    const { name, val } = next;
+    const setterName = `set${cap(name)}`;
+    return {
+      ...acc,
+      [name]: val,
+      [setterName]: (newState: Obj) => {
+        cb({
+          [name]: newState
+        });
+      }
+    };
+  }, {});
+}
+
+function main({ withState: stateDefs, onChange: cb }: Params) {
+  const state = reduceState(stateDefs, cb);
+  return state as Obj;
+}
+
+const cap = (string: string) =>
+  string.charAt(0).toUpperCase() + string.slice(1);
+
+export default main;
