069446dc084a38d0825734991f9313523f65e899
Thomas Collardeau <thomas@collardeau.com>
2018-04-01 18:45:06 +0200

    add ability to derive state

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,8 +1,41 @@
 import * as React from 'react';
 import * as renderer from 'react-test-renderer';
-import staterize from '../src/index';
+import s from '../src/index';
 
-test('start', () => {
-  const res = staterize();
-  expect(res).toBe('hello world');
+test('derives state', () => {
+  const defs = [
+    {
+      isZero: st => st.count === 0,
+      isOne: st => st.count === 1
+    }
+  ];
+  const derive = s(defs);
+  let t = derive({
+    a: 'a'
+  });
+  expect(t.a).toBe('a');
+  t = derive({
+    count: 0
+  });
+  expect(t.count).toBe(0);
+  expect(t.isZero).toBe(true);
+  expect(t.isOne).toBe(false);
+});
+
+test('', () => {
+  const defs = [
+    {
+      withBonus: st => st.bonus + st.count
+    }
+  ];
+  const derive = s(defs);
+  let t = derive(
+    {
+      count: 1
+    },
+    { bonus: 5 }
+  );
+  expect(t.count).toBe(1);
+  expect(t.withBonus).toBe(6);
+  expect(t.bonus).toBeUndefined();
 });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,27 @@
-function main() {
-  return 'hello world';
+import * as map from 'ramda/src/map';
+
+interface Obj {
+  [name: string]: any;
+}
+
+function derive(defs: Obj[], changes: Obj, prevState: Obj) {
+  return defs.reduce((acc: any, next: any) => {
+    const state = {
+      ...prevState,
+      ...changes
+    };
+    const dState = map((x: any) => x(state), next);
+    return {
+      ...acc,
+      ...dState
+    };
+  }, changes);
+}
+
+function main(defs: Obj[]) {
+  return function(changes: Obj, prevState: Obj = {}) {
+    return derive(defs, changes, prevState);
+  };
 }
 
 export default main;
